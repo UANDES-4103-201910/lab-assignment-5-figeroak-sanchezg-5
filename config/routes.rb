f8c908d67f24cf68398eba9a15df9a735a535c43
mpsanchez2@miuandes.cl <mapisanchezg@gmail.com>
2019-04-10 15:47:49 -0400

    feat(user):create user_with_most_tickets method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :tickets
   resources :event_venues
-  resources :events
-  resources :usersd
+  resources :events do
+    collection do
+      get :upcoming_events
+    end
+  end
+  resources :users do
+    collection do
+      get :users_with_most_tickets
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
