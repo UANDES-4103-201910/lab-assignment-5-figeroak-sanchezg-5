2a0a89b020d9c7bde37beff2536c3ac1f4777e28
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-04-10 11:51:06 -0400

    nested resources of ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -27,4 +27,15 @@ class TicketsController < ApplicationController
   def ticket_params
     params.require(:ticket).permit(:ticket_type_id, :order_id)
   end
+
+  def index
+    if params[:ticket_type_id]
+      @tickets = Ticket.where(ticket_type_id:params[:ticket_type_id])
+      respond_to do |format|
+        format.json {
+          render json:@tickets.to_json
+        }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :tickets
   resources :event_venues
   resources :events
-  resources :usersd
+  resources :users
+  resources :ticket_types do
+    resources :tickets
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
